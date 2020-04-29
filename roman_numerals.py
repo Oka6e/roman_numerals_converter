5ea586b28286bbc9310454a509b0971ef698d455
Oka6e <frankquocho214@gmail.com>
2020-04-29 10:24:00 -0700

    4/28 Commit (forgot)

--- roman_numerals.py
+++ roman_numerals.py
@@ -2,27 +2,46 @@ class RomanNumeral:
     def __init__(self, number):
         self.number = number
         self.int_num = 0
-    
-    def roman_to_int(self):
+        self.rom_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+
+    def isroman(self):
+        if self.IXC_triple_rules():
+            return False
         for i in self.number:
-            if i is "I":
-                self.int_num += 1
-            elif i is "V":
-                self.int_num += 5
-            elif i is "X":
-                self.int_num += 10
-            elif i is "L":
-                self.int_num += 50
-            elif i is "C":
-                self.int_num += 100
-            elif i is "D":
-                self.int_num += 500
-            elif i is "M":
-                self.int_num += 1000
-            else:
-                print("Please enter I, V, X, L, C, D, and/or M only.")
+            if i not in self.rom_dict.keys():
+                return False
+        return True
+
+    def roman_or_int(self):
+        if self.isroman():
+            print(self.roman_to_int())
+        elif self.number.isdigit():
+            print("number")
+        else:
+            print("Entry is neither roman numerals or integers.")
+    
+    def IXC_triple_rules(self):
+        if "IIII" in self.number or "XXXX" in self.number or "CCCC" in self.number:
+            print("You cannot have more than three I, X, or Cs in a row")
+
+    def roman_to_int(self):
+        rom_values = list(self.rom_dict.items())
+        for i in range(len(self.number) - 1):
+            for j in range(len(rom_values)):
+                if self.number[i] == rom_values[j][0]:
+                    self.int_num += rom_values[j][1] 
+                    for k in range(j+1, len(rom_values)):
+                        if self.number[i+1] == rom_values[k][0]:
+                            self.int_num -= 2*rom_values[j][1]
+                            break
+        for l in range(len(rom_values)):
+            if self.number[-1] == rom_values[l][0]:
+                self.int_num += rom_values[l][1]
         return self.int_num
 
-user_input = input("Enter a number in roman numerals: ")
-x = RomanNumeral(user_input)
-print(x.roman_to_int())
+while True:
+    user_input = input("Enter a number in roman numerals(ENTER to exit): ")
+    if user_input == "":
+        exit()
+    x = RomanNumeral(user_input)
+    x.roman_or_int()
