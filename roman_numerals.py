d16710428eadbe7f13faf5a9378c6f9ae62a5236
Oka6e <frankquocho214@gmail.com>
2020-05-01 23:53:06 -0700

    N/A

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,12 +1,27 @@
 class RomanNumeral:
     """A class to represent our roman numerals or integers."""
 
-    def __init__(self, number):
+    def __init__(self, number): 
         """Initializes the user's input, a counter, and standard values for each roman numeral."""
         self.number = number
         self.int_num = 0
-        self.rom_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        self.rom_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000} 
 
+    def roman_to_int(self): 
+        """Takes in user_input in roman numerals and return the equivalent integer."""
+        rom_values = list(self.rom_dict.items())
+        for i in range(len(self.number) - 1):
+            for j in range(len(rom_values)):
+                if self.number[i] == rom_values[j][0]:
+                    self.int_num += rom_values[j][1] 
+                    for k in range(j+1, len(rom_values)):
+                        if self.number[i+1] == rom_values[k][0]:
+                            self.int_num -= 2*rom_values[j][1]
+        for l in range(len(rom_values)):
+            if self.number[-1] == rom_values[l][0]:
+                self.int_num += rom_values[l][1]
+        return self.int_num
+        
     def isroman(self):
         """Checks if user input is a roman numeral."""
         for i in self.number:
@@ -34,24 +49,8 @@ class RomanNumeral:
     def non_subtractive_numberals(self):
         """Tag for V, L, and D. Can never be subtracted."""
 
-
-    def roman_to_int(self):
-        """Takes in user_input in roman numerals and return the equivalent integer."""
-        rom_values = list(self.rom_dict.items())
-        for i in range(len(self.number) - 1):
-            for j in range(len(rom_values)):
-                if self.number[i] == rom_values[j][0]:
-                    self.int_num += rom_values[j][1] 
-                    for k in range(j+1, len(rom_values)):
-                        if self.number[i+1] == rom_values[k][0]:
-                            self.int_num -= 2*rom_values[j][1]
-        for l in range(len(rom_values)):
-            if self.number[-1] == rom_values[l][0]:
-                self.int_num += rom_values[l][1]
-        return self.int_num
-
 while True:
-    user_input = input("Enter a number in roman numerals(ENTER to exit): ")
+    user_input = input("Enter a number in roman numerals: ")
     if user_input == "":
         exit()
     x = RomanNumeral(user_input)
