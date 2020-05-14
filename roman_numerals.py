692fc90863c1246b9c2c3ccaafacef5930461b8e
Frank Ho <frankquocho214@gmail.com>
2020-05-13 19:08:24 -0700

    Fixed bugs with updated algorithms.

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,123 +1,69 @@
 class RomanNumeral: 
     """A class to represent our roman numerals or integers."""
-    def __init__(self, number): 
+    def __init__(self, user_input): 
         """Initializes the user's input, a counter, and standard values for each roman numeral."""
-        self.number = number
-        self.int_num = 0 # counter
-        self.rom_num = ''
-        self.rom_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000} 
+        self.input = user_input
+        self.rom_dict = {u'M\u0305': 1000000, u'C\u0305M\u0305': 900000, u'D\u0305': 500000, \
+                    u'C\u0305D\u0305': 400000, u'C\u0305': 100000, u'X\u0305C\u0305': 90000, \
+                    u'L\u0305': 50000, u'X\u0305L\u0305': 40000, u'X\u0305': 10000, \
+                    u'I\u0305X\u0305': 9000, u'V\u0305': 5000, u'I\u0305V\u0305': 4000, 'M': 1000, \
+                    'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, \
+                    'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
 
-    def rom_to_int(self): # next() function? LXD and DXL give wrong answers.
-        """Takes in user_input in roman numerals and return the equivalent integer."""
-        for i in range(len(self.number)-1): # iterates through the string of roman numerals.
-            # subtraction rules
-            if self.number[i] == 'I' and (self.number[i+1] == 'V' or self.number[i+1] == 'X'):
-                self.int_num -= 1
-                continue
-            elif self.number[i] == 'X' and (self.number[i+1] == 'L' or self.number[i+1] == 'C'):
-                self.int_num -= 10
-                continue
-            elif self.number[i] == 'C' and (self.number[i+1] == 'D' or self.number[i+1] == 'M'):
-                self.int_num -= 100
-                continue
-            # or add roman numerals normally
+    def rom_or_int(self):
+            """Checks if user input is a roman numeral, integer, or not."""
+            if self.input.isdigit():
+                number = int(self.input)
+                print("The equivalent Roman Numeral is: " + self.int_to_rom(number) + ".\n")
+            elif self.isroman():
+                self.rom_to_int()
             else:
-                for k, v in self.rom_dict.items():
-                    if self.number[i] == k:
-                        self.int_num += v
+                print("Entry is neither in roman numerals or an integer.\n")
 
-        # Add the last roman numeral
-        for k,v in self.rom_dict.items():
-            if self.number[-1] == k:
-                self.int_num += v
-  
-        return self.int_num
-    
-    def int_to_rom(self):
-        """Takes the user input of integer and converts it to Roman Numerals."""
-        self.rom_lst = list(self.rom_dict.items()) # creates a list of the values of roman numerals
-        rev_rom_lst = self.rom_lst[::-1] # reverse the list order from greatest to least
-        number = int(self.number)
-        lst_len = len(rev_rom_lst)
-        i = 0
-        while i < lst_len:
-            if number - rev_rom_lst[i][1] >= 0:
-                number -= rev_rom_lst[i][1]
-                self.rom_num += rev_rom_lst[i][0]
-            elif i%2 == 0 and i < lst_len - 2 and number - (rev_rom_lst[i][1] - rev_rom_lst[i+2][1]) >= 0:
-                number -= rev_rom_lst[i][1] - rev_rom_lst[i+2][1]
-                self.rom_num += rev_rom_lst[i+2][0] + rev_rom_lst[i][0]
-                i += 1
-            elif i < lst_len - 1  and number - (rev_rom_lst[i][1] - rev_rom_lst[i+1][1]) >= 0:
-                number -= rev_rom_lst[i][1] - rev_rom_lst[i+1][1]
-                self.rom_num += rev_rom_lst[i+1][0] + rev_rom_lst[i][0]
-                i += 1
-            else:
-                i += 1
-        return self.rom_num
-        
     def isroman(self):
         """Checks if user input is a roman numeral."""
-        for i in self.number:
+        for i in self.input:
             if i not in self.rom_dict.keys():
                 return False
-        if self.IXC_triple_rules():
-            return False
-        elif self.precedence_rules():
-            return False
         return True
 
-    def roman_or_int(self):
-        """Checks if user input is a roman numeral, integer, or not."""
-        if self.number.isdigit():
-            print(self.int_to_rom())
-        elif self.isroman():
-            print("The integer value is: " + str(self.rom_to_int()) + ".\n")
+    def int_to_rom(self, number):
+        """Takes the user input of integer and converts it to Roman Numerals."""
+        # Create a list of tuples for the standard values of the roman numerals
+        roman_numeral = "" # empty string to create roman numeral
+
+        for k,v in self.rom_dict.items():
+            if number - v >= 0:
+                x = number // v
+                number -= x*v
+                roman_numeral += x * k
+        return roman_numeral
+            
+    def rom_to_int(self): # next() function? LXD and DXL give wrong answers.
+        """Takes in user_input in roman numerals and return the equivalent integer."""
+        rom_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+        integer = 0 # counter
+
+        for i in range(len(self.input)):
+            roman_value = rom_dict[self.input[i]]
+            if i+1 < len(self.input) and roman_value < rom_dict[self.input[i+1]]:
+                integer -= roman_value
+            else:
+                integer += roman_value
+        
+        check_value = self.int_to_rom(integer)
+        if self.input == check_value:
+            print("The equivalent integer value is: " + str(integer) + ".\n")
+            return integer
         else:
-            print("Entry is neither in roman numerals or an integer.\n")
-    
-    def IXC_triple_rules(self):
-        """Tag if user input has more than 3 I, X, or C in a row."""
-        if "IIII" in self.number or "XXXX" in self.number or "CCCC" in self.number:
-            print("You cannot have more than three I, X, or Cs in a row")
-            return True
-
-    # def VLD_double_rules(self):
-    #     """Tag if user input has more than 2 V, L , or D in a row."""
-    #     if "VV" in self.number or "LL" in self.number or "DD" in self.number:
-    #         print("You cannot have more than two V, L, or Ds in a row")
-    #         return True
-
-
-    # {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000} 
-    def precedence_rules(self):
-        for i in range(len(self.number) - 1):
-            if self.number[i] == 'I' and (self.number[i+1] == 'L' or self.number[i+1] == 'C' or \
-                self.number[i+1] == 'D' or self.number[i+1] == 'M'):
-                print("'I' cannot precede 'L', 'C', 'D', and 'M'.")
-                return True
-            elif self.number[i] == 'X' and (self.number[i+1] == 'D' or self.number[i+1] == 'M'):
-                print("'X' can only precede itself, 'I', 'V', 'L', and 'C'.")
-                return True
-            elif self.number[i] == 'C' and (self.number[i+1] == 'C' or self.number[i+1] != 'D' or self.number[i+1] != 'M'):
-                print("'C' can only precede itself, 'D', and 'M'.")
-                return True
-            elif self.number[i] == 'V' and self.number[i+1] != 'I':
-                print("'V' can only precede 'I'.")
-                return True
-            elif self.number[i] == 'L' and (self.number[i+1] != 'I' or self.number[i+1] != 'V' or \
-                self.number[i+1] != 'X'):
-                print("'L' can only precede 'I', 'V', or 'X'.")
-                return True
-            elif self.number[i] == 'D' and (self.number[i+1] == 'D' or self.number[i+1] == 'M'):
-                print("'V' can only precede 'I', 'V', 'X', 'L', or 'C'.")
-                return True
-        return False
+            print("This is not a proper roman numeral.\n")
 
 if __name__ == '__main__':
     while True:
-        user_input = input("Enter a number in roman numerals: ")
+        print("Press 'Enter' without an input to exit program.")
+        user_input = (input("Enter a Roman Numberal or integer to convert: "))
         if user_input == "":
             exit()
         x = RomanNumeral(user_input)
-        x.roman_or_int()
+        x.rom_or_int()
